3d1ff2ccfed56096ea547dbd5dfc45799b658784
Spikiwiky <dragon.spiki@gmail.com>
2025-04-08 01:03:41 +0700

    commit 8/4/2025

--- BepKhoiBackend/BepKhoiBackend.BusinessObject/obj/Debug/net6.0/BepKhoiBackend.BusinessObject.AssemblyInfo.cs
+++ BepKhoiBackend/BepKhoiBackend.BusinessObject/obj/Debug/net6.0/BepKhoiBackend.BusinessObject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BepKhoiBackend.BusinessObject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd6d265124521117f745abeb90be15840128adc5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65455e7a3d1038c15767f74eee2b44279a415e52")]
 [assembly: System.Reflection.AssemblyProductAttribute("BepKhoiBackend.BusinessObject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BepKhoiBackend.BusinessObject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
